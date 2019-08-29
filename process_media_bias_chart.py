d1571752c70f8b0b5b21c68c18cc903c5f0c8153
James Moore <james@panic.com>
2019-08-29 13:47:47 -0700

    split sites into buckets

--- process_media_bias_chart.py
+++ process_media_bias_chart.py
@@ -6,6 +6,9 @@ from urllib.parse import urlparse
 input_csv = 'Interactive Media Bias Chart - Ad Fontes Media.csv'
 sites = {}
 
+"""
+Collect the data
+"""
 line = 0
 with open(input_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -18,25 +21,76 @@ with open(input_csv) as csv_file:
                 parsed_uri = urlparse(url)
 
                 sites[site] = {
-                    'url':  f"{parsed_uri.scheme}://{parsed_uri.netloc}",
-                    'b': [float(bias)],
-                    'q': [float(quality)]
+                    'url': f"{parsed_uri.scheme}://{parsed_uri.netloc}",
+                    'b':   [float(bias)],
+                    'q':   [float(quality)]
                 }
             else:
                 sites[site]['b'].append(float(bias))
                 sites[site]['q'].append(float(quality))
         line += 1
 
+l_biases = []
+r_biases = []
+
+sites_to_save = []
+sites_to_remove = [
+    'The Skimm',
+    'Jacobin',
+    'Vanity Fair',
+    'OZY',
+    'Weather.com',
+]
+
+"""
+Clean out sites we don't want and figure out where to split the buckets
+"""
+for site, data in sites.items():
+    q_med = statistics.median(data['q'])
+
+    if q_med >= 32.0 and site not in sites_to_remove:
+        b_med = statistics.median(data['b'])
+
+        if b_med >= 0:
+            r_biases.append(b_med)
+        else:
+            l_biases.append(b_med)
+
+        data = (site, sites[site]['url'], b_med, q_med)
+        sites_to_save.append(data)
+
+l_median = statistics.median(l_biases)
+r_median = statistics.median(r_biases)
+
+"""
+Split into buckets
+"""
+print("Left/Right Median", l_median, r_median)
+
+l2_sites = []
+l1_sites = []
+r1_sites = []
+r2_sites = []
+
+for site in sites_to_save:
+
+    if site[2] < l_median:
+        l2_sites.append(site)
+    elif site[2] < 0:
+        l1_sites.append(site)
+    elif site[2] < r_median:
+        r1_sites.append(site)
+    else:
+        r2_sites.append(site)
+
+print("Bucket Sizes", len(l2_sites), len(l1_sites), len(r1_sites), len(r2_sites), )
+
 with open('sites.csv', 'w') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Site', 'B', 'Q'])
+    csv_writer.writerow(['Site', 'URL', 'B', 'Q'])
 
-    for site, data in sites.items():
-        sites[site]['b_med'] = statistics.median(data['b'])
+    for site in sites_to_save:
+        csv_writer.writerow(site)
 
-        sites[site]['q_med'] = statistics.median(data['q'])
-
-        if sites[site]['q_med'] >= 32.0:
-            csv_writer.writerow([site, sites[site]['url'], sites[site]['b_med'], sites[site]['q_med']])
-
-
+with open('sites.json', 'w') as fp:
+    json.dump([l2_sites, l1_sites, r1_sites, r2_sites], fp, indent=4)
--- run.py
+++ run.py
@@ -1,20 +1,22 @@
+import json
 import platform
 from io import BytesIO
 from pathlib import Path
+from random import randrange
+
 from selenium import webdriver
 from PIL import Image
 import urllib.request
+import csv
 
 width = 1200
 height = 1000
 
 is_linux = platform.system() == 'Linux'
 
-if is_linux:
-    from pyvirtualdisplay import Display
-    display = Display(visible=0, size=(width, height))
-    display.start()
-
+"""
+Download uBlock
+"""
 ublock_filename = 'ublock_origin-1.21.2-an+fx.xpi'
 ublock = Path.joinpath(Path.cwd(), ublock_filename)
 if not ublock.exists():
@@ -22,54 +24,63 @@ if not ublock.exists():
     url = 'https://addons.mozilla.org/firefox/downloads/file/3361355/ublock_origin-1.21.2-an+fx.xpi'
     urllib.request.urlretrieve(url, ublock_filename)
 
+"""
+Start up the virtual framebuffer if we're on Linux
+"""
+if is_linux:
+    from pyvirtualdisplay import Display
+
+    display = Display(visible=0, size=(width, height))
+    display.start()
+
+"""
+Load Site data
+"""
+
+buckets = json.load(open('sites.json', 'r'))
+sites = []
+for site_i in range(4):
+    n = randrange(len(buckets[site_i]))
+    sites.append(buckets[site_i][n])
+
+"""
+Start Firefox and bump up the font size
+"""
 profile = webdriver.FirefoxProfile()
-
 profile.set_preference('font.minimum-size.x-western', 16)
-
 driver = webdriver.Firefox(firefox_profile=profile)
 driver.install_addon(ublock.as_posix(), temporary=False)
 
 images = []
+description = "Front pages of\n"
 
-driver.get("https://nytimes.com")
-print(driver.title)
-images.append(driver.get_screenshot_as_png())
-
-driver.get("https://www.washingtonpost.com/")
-print(driver.title)
-images.append(driver.get_screenshot_as_png())
-
-driver.get("https://foxnews.com/")
-print(driver.title)
-images.append(driver.get_screenshot_as_png())
-
-driver.get("https://cnn.com/")
-print(driver.title)
-images.append(driver.get_screenshot_as_png())
+"""
+Capture the site images
+"""
+for (name, url, b, q) in sites:
+    print(name, url)
+    driver.get(url)
+    cap = driver.get_screenshot_as_png()
+    im = Image.open(BytesIO(cap))
+    im.save(f'caps/{name}.png')
+    images.append(im)
+    description += f"{name}\n"
 
 driver.quit()
+
+print(description)
 
 if is_linux:
     display.stop()
 
-im = Image.open(BytesIO(images[0]))
-# im.save('caps/1.png')
-w, h = im.size
+"""
+Combine the site images
+"""
+w, h = images[0].size
 
-comp_image = Image.new('RGB', (w, h*4))
-comp_image.paste(im, (0, 0))
+comp_image = Image.new('RGB', (w, h * 4))
 
-im = Image.open(BytesIO(images[1]))
-# im.save('caps/2.png')
-comp_image.paste(im, (0, h))
-
-im = Image.open(BytesIO(images[2]))
-# im.save('caps/3.png')
-comp_image.paste(im, (0, h*2))
-
-im = Image.open(BytesIO(images[3]))
-# im.save('caps/4.png')
-comp_image.paste(im, (0, h*3))
+for i in range(4):
+    comp_image.paste(images[i], (0, (i * h)))
 
 comp_image.save(f'caps/comp-{width}-{height}.jpg')
-
