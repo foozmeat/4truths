c805570643782f57b53abb6e65e9f49935392dd7
James Moore <james@panic.com>
2019-08-30 16:58:30 -0700

    switch to 4 separate images

--- run.py
+++ run.py
@@ -1,18 +1,23 @@
 import json
+import os
 import platform
+import time
+import urllib.request
+from datetime import datetime
 from io import BytesIO
 from pathlib import Path
 from random import randrange
+from sys import stderr
 
-from selenium import webdriver
 from PIL import Image
-import urllib.request
-import csv
+from mastodon import Mastodon, MastodonAPIError, MastodonNetworkError
+from selenium import webdriver
 
 width = 1200
 height = 1000
 
 is_linux = platform.system() == 'Linux'
+debug = os.getenv('DEBUG', False)
 
 """
 Download uBlock
@@ -55,20 +60,41 @@ images = []
 description = "Front pages of\n"
 
 """
+create mastodon API
+"""
+m_config = json.load(open('config.json', 'r'))
+
+mast_api = Mastodon(
+        client_id=m_config['client_key'],
+        client_secret=m_config['client_secret'],
+        api_base_url=m_config['client_host'],
+        access_token=m_config['access_token'],
+        debug_requests=False,
+        request_timeout=15,
+        ratelimit_method='throw'
+)
+
+media_ids = []
+
+"""
 Capture the site images
 """
+date = datetime.now().strftime('%Y%m%d%H%M%S')
+cap_folder = Path(f'caps/{date}')
+cap_folder.mkdir()
+
 for (name, url, b, q) in sites:
     print(name, url)
     driver.get(url)
     cap = driver.get_screenshot_as_png()
     im = Image.open(BytesIO(cap))
-    im.save(f'caps/{name}.png')
+    cap_path = f'{cap_folder}/{name}.png'
+    im.save(cap_path)
     images.append(im)
-    description += f"{name}\n"
+    description = f"Front page of {name}\n"
+    media_ids.append(mast_api.media_post(cap_path, description=description))
 
 driver.quit()
-
-print(description)
 
 if is_linux:
     display.stop()
@@ -76,11 +102,36 @@ if is_linux:
 """
 Combine the site images
 """
-w, h = images[0].size
+# w, h = images[0].size
+#
+# comp_image = Image.new('RGB', (w, h * 4))
+#
+# for i in range(4):
+#     comp_image.paste(images[i], (0, (i * h)))
+#
+# comp_path = f'{cap_folder}/comp-{width}-{height}.jpg'
+# comp_image.save(comp_path)
 
-comp_image = Image.new('RGB', (w, h * 4))
+"""
+Send toot
+"""
 
-for i in range(4):
-    comp_image.paste(images[i], (0, (i * h)))
+if not debug:
+    posted = False
 
-comp_image.save(f'caps/comp-{width}-{height}.jpg')
+    while not posted:
+        try:
+            # media_id = [mast_api.media_post(comp_path, description=description)]
+
+            post = mast_api.status_post(
+                    u"\u2063",
+                    media_ids=media_ids,
+                    visibility='private',
+                    sensitive=False)
+
+            posted = True
+
+        except (MastodonAPIError, MastodonNetworkError) as e:
+            print(e, file=stderr)
+            time.sleep(15)
+
